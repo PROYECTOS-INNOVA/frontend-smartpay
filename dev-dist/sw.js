0c7718a54def858e6e630d9afbe64f245b5e2adf
JulianPDP <julianazange@gmail.com>
2025-07-14 20:52:26 -0500

    MOD: landingpage in public

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e7681877'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.nd1v28uacko"
+    "revision": "0.7q7c40ujrs8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
