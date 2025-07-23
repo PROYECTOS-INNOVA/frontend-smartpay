445ffe76495871b6a03467fdfb4b03ed3d9775c6
Julian-Azange <julianazange@gmail.com>
2025-07-23 17:10:58 -0500

    config del proviaconador - TENER EN CUENTA

--- src/features/config/ConfigurationPage.jsx
+++ src/features/config/ConfigurationPage.jsx
@@ -13,7 +13,7 @@ import Swal from 'sweetalert2';
 
 function ConfigurationPage() {
   const CLIENT_ID = "631597337466-dt7qitq7tg2022rhje5ib5sk0eua6t79.apps.googleusercontent.com";
-  const REDIRECT_URI = "https://smartpay-oficial.com:8000/api/v1/google/auth/callback";
+  const REDIRECT_URI = "https://smartpay-oficial.com:9443/api/v1/google/auth/callback";
   const SCOPE = "profile email https://www.googleapis.com/auth/userinfo.profile";
 
   const [accounts, setAccounts] = useState([]);
--- src/features/payments/components/Step2DeviceProvisioning.jsx
+++ src/features/payments/components/Step2DeviceProvisioning.jsx
@@ -21,11 +21,11 @@ const Step2DeviceProvisioning = ({ onNext, onBack, initialData = {} }) => {
 
   const [simulateDummyDevice, setSimulateDummyDevice] = useState(false);
 
-  const generateProvisioningJson = (enrolmentId) => ({
+const generateProvisioningJson = (enrolmentId) => ({
     "android.app.extra.PROVISIONING_DEVICE_ADMIN_COMPONENT_NAME": "com.olimpo.smartpay/com.olimpo.smartpay.receivers.SmartPayDeviceAdminReceiver",
-    "android.app.extra.PROVISIONING_DEVICE_ADMIN_SIGNATURE_CHECKSUM": "V2VCGou8gh_l_gy2PAJb5mN3Y5gLF4QetKHyEHR4eZc",
+    "android.app.extra.PROVISIONING_DEVICE_ADMIN_SIGNATURE_CHECKSUM": "JfkRLA-Lae1o0gGrvVUGi0M34pYu3eLFlgY-UHdsXbs=",
     "android.app.extra.PROVISIONING_DEVICE_ADMIN_PACKAGE_DOWNLOAD_LOCATION": "https://appincdevs.com/enterprise/smartpay-google.apk",
-    "android.app.extra.PROVISIONING_DEVICE_ADMIN_PACKAGE_CHECKSUM": "V2VCGou8gh_l_gy2PAJb5mN3Y5gLF4QetKHyEHR4eZc",
+    "android.app.extra.PROVISIONING_DEVICE_ADMIN_PACKAGE_CHECKSUM": "JfkRLA-Lae1o0gGrvVUGi0M34pYu3eLFlgY-UHdsXbs=",
     "android.app.extra.PROVISIONING_LEAVE_ALL_SYSTEM_APPS_ENABLED": true,
     "android.app.extra.PROVISIONING_LOCALE": "es_ES",
     "android.app.extra.PROVISIONING_ADMIN_EXTRAS_BUNDLE": {
