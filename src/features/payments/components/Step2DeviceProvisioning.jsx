d7ece2fd99994b1e1b9ed0e867e672931f43da35
juantobon20 <juantobon20@outlook.es>
2025-07-25 12:37:20 -0500

    Cambio de claves QR

--- src/features/payments/components/Step2DeviceProvisioning.jsx
+++ src/features/payments/components/Step2DeviceProvisioning.jsx
@@ -23,9 +23,9 @@ const Step2DeviceProvisioning = ({ onNext, onBack, initialData = {} }) => {
 
 const generateProvisioningJson = (enrolmentId) => ({
     "android.app.extra.PROVISIONING_DEVICE_ADMIN_COMPONENT_NAME": "com.olimpo.smartpay/com.olimpo.smartpay.receivers.SmartPayDeviceAdminReceiver",
-    "android.app.extra.PROVISIONING_DEVICE_ADMIN_SIGNATURE_CHECKSUM": "JfkRLA-Lae1o0gGrvVUGi0M34pYu3eLFlgY-UHdsXbs=",
+    "android.app.extra.PROVISIONING_DEVICE_ADMIN_SIGNATURE_CHECKSUM": "b-1JlJby48t_WGJVyYs-YdTrSoiBn-Xq5Ot0SgMjAYk=",
     "android.app.extra.PROVISIONING_DEVICE_ADMIN_PACKAGE_DOWNLOAD_LOCATION": "https://appincdevs.com/enterprise/smartpay-google.apk",
-    "android.app.extra.PROVISIONING_DEVICE_ADMIN_PACKAGE_CHECKSUM": "JfkRLA-Lae1o0gGrvVUGi0M34pYu3eLFlgY-UHdsXbs=",
+    "android.app.extra.PROVISIONING_DEVICE_ADMIN_PACKAGE_CHECKSUM": "b-1JlJby48t_WGJVyYs-YdTrSoiBn-Xq5Ot0SgMjAYk=",
     "android.app.extra.PROVISIONING_LEAVE_ALL_SYSTEM_APPS_ENABLED": true,
     "android.app.extra.PROVISIONING_LOCALE": "es_ES",
     "android.app.extra.PROVISIONING_ADMIN_EXTRAS_BUNDLE": {
