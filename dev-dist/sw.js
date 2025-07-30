7007b17c447a50cefdd7cdcdcd6bcc421199ba61
Julian-Azange <julianazange@gmail.com>
2025-07-30 15:02:08 -0500

    fix: s erestauran archivos

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e7681877'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kk9li78pf68"
+    "revision": "0.t7j9r20mk3o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
