bbb7dbd78934d5a9f38d71647e49298c396ef743
Julian-Azange <julianazange@gmail.com>
2025-07-30 15:09:08 -0500

    normalize line endings

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'; 
+import AppRoutes from './routes/AppRoutes';
+//App js
+function App() {
+  useEffect(() => {
+    const style = document.createElement('style');
+    style.innerHTML = `
+      .swal2-zindex-fix {
+        z-index: 10050 !important;
+      }
+      .swal2-container {
+        z-index: 10050 !important;
+      }
+      .swal2-backdrop-show {
+        z-index: 10049 !important;
+      }
+    `;
+    document.head.appendChild(style);
+  }, []);
+
+  return (
+    <AppRoutes />
+  );
+}
+
+export default App;
