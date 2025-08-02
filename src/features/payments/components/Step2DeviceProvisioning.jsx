04aba72094eade79ab1e1f2ae038c1ad162c68d9
juantobon20 <juantobon20@outlook.es>
2025-08-02 17:04:24 -0500

    Cambio checksum

--- src/features/payments/components/Step2DeviceProvisioning.jsx
+++ src/features/payments/components/Step2DeviceProvisioning.jsx
@@ -22,9 +22,9 @@ const Step2DeviceProvisioning = ({ onNext, onBack, initialData = {} }) => {
 
 const generateProvisioningJson = (enrolmentId, storeId) => ({
     "android.app.extra.PROVISIONING_DEVICE_ADMIN_COMPONENT_NAME": "com.olimpo.smartpay/com.olimpo.smartpay.receivers.SmartPayDeviceAdminReceiver",
-    "android.app.extra.PROVISIONING_DEVICE_ADMIN_SIGNATURE_CHECKSUM": "oN0D4_SyeQlPs-w7rWcq6ZAMEHiKsQGz_0-pOAaeDOc=",
+    "android.app.extra.PROVISIONING_DEVICE_ADMIN_SIGNATURE_CHECKSUM": "JqFx0c1sP2qpgmtXcDf6wdbjqb9gYB5oN-r_TjSkdB4=",
     "android.app.extra.PROVISIONING_DEVICE_ADMIN_PACKAGE_DOWNLOAD_LOCATION": "https://appincdevs.com/enterprise/smartpay-google.apk",
-    "android.app.extra.PROVISIONING_DEVICE_ADMIN_PACKAGE_CHECKSUM": "oN0D4_SyeQlPs-w7rWcq6ZAMEHiKsQGz_0-pOAaeDOc=",
+    "android.app.extra.PROVISIONING_DEVICE_ADMIN_PACKAGE_CHECKSUM": "JqFx0c1sP2qpgmtXcDf6wdbjqb9gYB5oN-r_TjSkdB4=",
     "android.app.extra.PROVISIONING_LEAVE_ALL_SYSTEM_APPS_ENABLED": true,
     "android.app.extra.PROVISIONING_LOCALE": "es_ES",
     "android.app.extra.PROVISIONING_ADMIN_EXTRAS_BUNDLE": {
